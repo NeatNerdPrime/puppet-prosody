8d76849acf0761489886ce52ad4d71736dad9d6a
artonix101 <artinab@posteo.de>
2025-12-29 16:50:59 +0100

    add further deps test

--- spec/classes/prosody_spec.rb
+++ spec/classes/prosody_spec.rb
@@ -75,6 +75,7 @@ describe 'prosody' do
       context 'with community modules' do
         let(:params) { { community_modules: %w[coolmod othermod] } }
 
+        it { is_expected.to compile.with_all_deps }
         it {
           is_expected.to contain_file('/etc/prosody/prosody.cfg.lua'). \
             with_content(%r{"[^"]+/mod_coolmod";}). \
